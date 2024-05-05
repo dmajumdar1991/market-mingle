9fc95c5ba18ce5003258442b84b0c726b98c7ed9
Dipankar Majumdar <dipmajumdar1991@gmail.com>
2024-05-05 12:53:50 +0530

    redux configuration done

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+);
